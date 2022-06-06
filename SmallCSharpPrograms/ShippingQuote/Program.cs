e7744342c32303c1d38b99bfa9a75b95b2e5f8af
BeeIsland <cameronvwa@gmail.com>
2022-06-06 14:02:48 -0700

    Added mail price calc app

---
+++ SmallCSharpPrograms/ShippingQuote/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+        Console.WriteLine("How heavy is the package in pounds");
+        int pckgWeight = Convert.ToInt32(Console.ReadLine());
+        if (pckgWeight > 50)
+        { 
+            Console.WriteLine("Package is too heavy to be shipped via Package Express. Have a good day");
+        }
+        else
+        {
+            Console.WriteLine("What is the width of the package?");
+            int pckgWidth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is the height of the package?");
+            int pckgHeight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is the length of the package?");
+            int pckgLength = Convert.ToInt32(Console.ReadLine());
+            if (pckgWidth + pckgHeight + pckgLength > 50) 
+            {
+                Console.WriteLine("Package is too big to be shipped via Package Express. Have a good day");
+            }
+            else
+            {
+                int quote = (pckgLength * pckgHeight * pckgWidth * pckgWeight)/100;
+                Console.WriteLine("Your quote is : $" + quote.ToString() + ".00");
+            }
+        }
+        Console.ReadLine();
+    }
+}
+
