9736f6baede5e73107fd6d1ffae3425823b522db
BeeIsland <cameronvwa@gmail.com>
2022-06-06 10:57:28 -0700

    Added a daily report app

---
+++ DailyReport/DailyReport/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{	
+	static void Main()
+	{
+
+		Console.WriteLine("The Tech Academy\nStudent Daily Report\n");
+
+		Console.WriteLine("What is your name?");
+		string fName = Console.ReadLine();
+
+		Console.WriteLine("What course are you on?");
+		string courseName = Console.ReadLine();
+
+		Console.WriteLine("What page number?");
+		int pageNum = Convert.ToInt32(Console.ReadLine());
+
+		Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\"");
+		string answer = Console.ReadLine();
+		bool needHelp;
+		if (answer == "true")
+        {
+			needHelp = true;
+        }
+		else
+        {
+			needHelp = false;
+        }
+
+		Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+		string posExp = Console.ReadLine();
+
+		Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+		string feedback = Console.ReadLine();
+
+		Console.WriteLine("How many hours did you study today?");
+		int hrsStudied = Convert.ToInt32(Console.ReadLine());
+
+		Console.WriteLine("Thank you for your answers.\nAn Instructor will respond to this shortly.\nHave a great day!");
+		Console.ReadLine();
+	}
+}
