50264cf97255bf16799e1420a2bd472e812ff6fa
BeeIsland <cameronvwa@gmail.com>
2022-06-08 13:01:35 -0700

    Created the groundwork for a simple card game

---
+++ SmallCSharpPrograms/TwentyOne/Deck.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Deck
+    {
+        public Deck()
+        {
+            Cards = new List<Card>();
+            List<string> Suits = new List<string>() { "Clubs", "Hearts", "Diamonds", "Spades"};
+            List<string> Faces = new List<string>() 
+            {
+                "Ace", "Two", "Three", "Four","Five","Six","Seven","Eight","Nine","Ten",
+                "Jack","Queen","King"
+            };
+
+            foreach (string suit in Suits)
+            {
+                foreach (string face in Faces)
+                {
+                    Card card = new Card();
+                    card.Suit = suit;
+                    card.Face = face;
+                    Cards.Add(card);
+                }
+            }
+
+
+        }
+        public List<Card> Cards { get; set; }
+    }
+}
---
+++ SmallCSharpPrograms/TwentyOne/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main()
+        {
+            Deck deckOne = new Deck();
+
+            deckOne = Shuffle(deckOne);
+
+            foreach (Card card in deckOne.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.ReadLine();
+        }
+
+        public static Deck Shuffle(Deck deck)
+        {
+            List<Card> tempList = new List<Card>();
+            Random random = new Random();
+
+            while (deck.Cards.Count > 0)
+            {
+                int randomIndex = random.Next(0, deck.Cards.Count);
+                tempList.Add(deck.Cards[randomIndex]);
+                deck.Cards.RemoveAt(randomIndex);
+            }
+            deck.Cards = tempList;
+            return deck;
+        }
+    }
+}
