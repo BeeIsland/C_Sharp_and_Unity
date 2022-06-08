2af2b21ac9f004c1df7a24233f026de2daa433f9
BeeIsland <cameronvwa@gmail.com>
2022-06-08 13:01:19 -0700

    Created a class and some functions

---
+++ SmallCSharpPrograms/MethodsAndClasses/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number to do math on.");
+            string rawInput = Console.ReadLine();
+            int numToMath = Convert.ToInt32(rawInput);
+            Console.WriteLine("Square of number = " + mathFunctions.Square(numToMath));
+            Console.WriteLine("Cube of number = " + mathFunctions.Cube(numToMath));
+            Console.WriteLine("Factorial of number = " + mathFunctions.Factorial(numToMath));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SmallCSharpPrograms/MethodsAndClasses/mathFunctions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndClasses
+{
+    internal class mathFunctions
+    {
+        public static int Square(int number)
+        {
+            return number * number;
+        }
+
+        public static int Cube(int number)
+        {
+            return number * number * number;
+        }
+
+        public static int Factorial(int number)
+        {
+            int total = 1;
+            for (int i = 0; i < number; i++)
+            {
+                total *= number-i;
+            }
+            return total;
+        }
+    }
+}
