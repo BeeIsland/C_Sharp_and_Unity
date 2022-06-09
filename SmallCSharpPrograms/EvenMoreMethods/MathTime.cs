9be8bf78a99c25e0ba4de26c740ddd56c72c8050
BeeIsland <cameronvwa@gmail.com>
2022-06-09 11:37:09 -0700

    Tested out optional parameters

---
+++ SmallCSharpPrograms/EvenMoreMethods/MathTime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenMoreMethods
+{
+    internal class MathTime
+    {
+        public int Multiply(int num1, int num2 = 10)
+        {
+            return num1 * num2;
+        }
+    }
+}
---
+++ SmallCSharpPrograms/EvenMoreMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenMoreMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathTime mathTime = new MathTime();
+            Console.WriteLine("Input a number to multiply");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("If you want to multiply by a number other than 10, please input it now\nOtherwise, leave it blank and press enter");
+            string rawNum = Console.ReadLine();
+
+            if (rawNum == "")
+            {
+                Console.WriteLine(mathTime.Multiply(num1));
+            }
+            else
+            {
+                Console.WriteLine(mathTime.Multiply(num1, Convert.ToInt32(rawNum)));
+            }
+            Console.ReadLine();
+        }
+    }
+}
