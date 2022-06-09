c0358b4a125ad30aaeb14415e6cbbe578377cde4
BeeIsland <cameronvwa@gmail.com>
2022-06-09 11:20:21 -0700

    Made a small math program to test some methods

---
+++ SmallCSharpPrograms/MoreMethods/MathOperation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreMethods
+{
+    internal class MathOperation
+    {
+        public int Operate(int number)
+        {
+            return number + 20;
+        }
+
+        public decimal Operate(decimal number)
+        {
+            return number * 10m;
+        }
+
+        public int Operate(string number)
+        {
+            int answer = Convert.ToInt32(number);
+            return answer / 2;
+        }
+    }
+}
---
+++ SmallCSharpPrograms/MoreMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoreMethods
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            MathOperation test = new MathOperation();
+            Console.WriteLine(test.Operate(10));
+            Console.WriteLine(test.Operate(10m));
+            Console.WriteLine(test.Operate("10"));
+            Console.ReadLine();
+        }
+    }
+}
