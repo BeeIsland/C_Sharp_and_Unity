50347e98ea3153bf0e5e6e6de8b76ff06fdd5f68
BeeIsland <cameronvwa@gmail.com>
2022-06-10 10:20:03 -0700

    Created small program to test void methods

---
+++ SmallCSharpPrograms/MoreMethods2/InvisMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreMethods2
+{
+    internal class InvisMath
+    {
+        public  void NumDisplay(int num1, int num2)
+        {
+            num1 *= 10;
+            Console.WriteLine(num2);
+            return;
+        }
+    }
+}
---
+++ SmallCSharpPrograms/MoreMethods2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreMethods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InvisMath numbers = new InvisMath();
+            numbers.NumDisplay(10, 20);
+            numbers.NumDisplay(num1: 30, num2: 40);
+            Console.ReadLine();
+        }
+    }
+}
