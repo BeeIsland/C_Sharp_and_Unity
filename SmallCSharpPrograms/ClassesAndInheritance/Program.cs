49328a00cc34c7c9b5d5935d0de3d5c05235165d
BeeIsland <cameronvwa@gmail.com>
2022-06-10 11:10:09 -0700

    Created program to test inheritance

---
+++ SmallCSharpPrograms/ClassesAndInheritance/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndInheritance
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student"};
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
