ee1a12b5ac02a199cbe33f6fd9d8ed44f74e3f15
BeeIsland <cameronvwa@gmail.com>
2022-06-10 12:59:02 -0700

    Testing out polymorphism

---
+++ SmallCSharpPrograms/Polymorphism/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+
+        public void Quit()
+        {
+            FirstName = "Not";
+            LastName = "Applicable";
+            SayName();
+        }
+    }
+}
---
+++ SmallCSharpPrograms/Polymorphism/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            IQuittable employee1 = new Employee();
+            employee1.Quit();
+            Console.ReadLine();
+
+            Employee employee2 = new Employee();
+            employee2.Quit();
+            Console.ReadLine();
+        }
+    }
+}
