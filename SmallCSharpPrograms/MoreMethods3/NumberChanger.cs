5bdde18c62050ce87452622c2a0e84e9b48bd5d3
BeeIsland <cameronvwa@gmail.com>
2022-06-10 10:48:52 -0700

    Tested static classes and more methods

---
+++ SmallCSharpPrograms/MoreMethods3/NumberChanger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreMethods3
+{
+    static class NumberChanger
+    {
+        public static void Division(int num1, out int answer)
+        {
+            answer = num1 / 2;
+        }
+
+        public static void OutputTest(string input, out string output)
+        {
+            output = input.ToUpper();
+        }
+
+        public static void Division(int num1, int dividingNum, out int answer)
+        {
+            answer = num1 / dividingNum;
+        }
+    }
+}
---
+++ SmallCSharpPrograms/MoreMethods3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreMethods3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please input a number");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            NumberChanger.Division(userNum, out int answer);
+            Console.WriteLine(userNum + " / 2 = " + answer);
+
+            Console.WriteLine("Please input a sentence");
+            string userString = Console.ReadLine();
+            NumberChanger.OutputTest(userString, out string outputString);
+            Console.WriteLine("UpperCase: " + outputString);
+
+            Console.WriteLine("Please input a number to divide");
+            int userSecondNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please input a number to divide that number by");
+            int dividingNum = Convert.ToInt32(Console.ReadLine());
+            NumberChanger.Division(userSecondNum, dividingNum, out int outputNum);
+            Console.WriteLine(userSecondNum + " / " + dividingNum + " = " + outputNum);
+
+            Console.ReadLine();
+        }
+    }
+}
