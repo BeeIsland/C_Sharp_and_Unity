a9e161537722332813c3d403328fe1ec94c78fb6
BeeIsland <cameronvwa@gmail.com>
2022-06-15 13:32:32 -0700

    Added a program to test lambda expressions

---
+++ SmallCSharpPrograms/LambdaExpressions/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Employee : Person
+    {
+        public int EmployeeID { get; set; }
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+}
