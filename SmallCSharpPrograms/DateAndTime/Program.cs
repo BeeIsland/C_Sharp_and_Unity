072819a3f2295d6813f8b5e06e482fc982168f16
BeeIsland <cameronvwa@gmail.com>
2022-06-21 13:23:09 -0700

    Created an app for adding hours to current time
    should the number of hours left to add be larger than the number of hours left in the day, it will simply add a day.
    The same applies to years

---
+++ SmallCSharpPrograms/DateAndTime/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateAndTime
+{
+    public class Program
+    {
+        static void Main()
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now.ToString());
+            Console.ReadLine();
+            Console.WriteLine("Give me a number");
+            int addNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("In {0} hours it will be {1}", addNum, now.AddHours(addNum));
+            Console.ReadLine();
+        }
+    }
+}
