47fc7fdcd827fc627c692ae885d6883c48dedd4c
BeeIsland <cameronvwa@gmail.com>
2022-06-22 12:31:51 -0700

    Added a few tweaks

--- SmallCSharpPrograms/TwentyOne/Program.cs
+++ SmallCSharpPrograms/TwentyOne/Program.cs
@@ -12,13 +12,14 @@ namespace TwentyOne
         static void Main()
         {
 
+
             Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
             string playerName = Console.ReadLine();
 
             Console.WriteLine("And how much money did you bring today?");
             int bank = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
+            Console.WriteLine("Hello, {0}. Would you like to play some Blackjack?", playerName);
             string answer = Console.ReadLine().ToLower();
 
             if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
